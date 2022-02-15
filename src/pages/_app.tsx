05bec65feb3123399270ed1e674a96188411b93b
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-02-15 22:56:02 +0700

    feat: move inter font import to custom document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,10 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
 import { SessionProvider } from 'next-auth/react'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <>
-      <Head>
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
-      </Head>
       <SessionProvider session={session}>
         <Component {...pageProps} />
       </SessionProvider>
