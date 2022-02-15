3395a8c8b7a2a25ff1d2a25edaa0646dad99b646
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-02-16 01:38:13 +0700

    fix: component inside module not working with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/common/components/**/*.{js,ts,jsx,tsx}',
+    './src/modules/**/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
