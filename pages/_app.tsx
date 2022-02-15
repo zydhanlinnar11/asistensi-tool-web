15c377e652026bd04d0fc622f108928ca50a0183
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-02-15 17:02:32 +0700

    feat: configure Inter font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+  <Head>
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
+  </Head>
+  <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
