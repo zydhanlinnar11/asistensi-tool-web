0d4d7624f9e5d8fb1fb1b51f7b2dfd15b1f9d699
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-02-15 17:42:52 +0700

    feat: move pages to src directory

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,14 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <>
-  <Head>
-    <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
-  </Head>
-  <Component {...pageProps} />
-  </>
+  return (
+    <>
+      <Head>
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
