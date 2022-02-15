44afb2e5a7863e9836e6c42c150e135a39d29ecc
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-02-15 22:21:01 +0700

    feat(auth): add dashboard endpoint

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,7 +2,6 @@ import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
 export default NextAuth({
-  secret: process.env.SECRET ?? '',
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
---
+++ src/pages/asdos/dashboard.tsx
@@ -0,0 +1,1 @@
+export { default } from '@/modules/asdos/components/Dashboard'
