90e30942d5ba4171df9a50897ec3cd3d4f0e12b9
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-02-17 00:35:07 +0700

    feat: initialize dashboard for asisten

--- src/modules/asdos/components/Dashboard.tsx
+++ src/modules/asdos/components/Dashboard.tsx
@@ -3,7 +3,9 @@ import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import Header from '@/common/components/elements/Header'
 import { useUser } from '@/modules/auth/providers/UserProvider'
-import PrivateRoute from '@/common/components/PrivateRoute'
+import PrivateRoute from './PrivateRoute'
+import Link from 'next/link'
+import Card from '@/common/components/elements/Card'
 
 const Dashboard: NextPage = () => {
   const { user } = useUser()
@@ -18,22 +20,15 @@ const Dashboard: NextPage = () => {
 
         <Header
           midText={`Dashboard Asisten`}
-          bottomText={`Selamat datang, ${user?.name}!`}
+          bottomText={`Selamat datang asisten kelas ${user?.kelas}, ${user?.name} (${user?.nrp})!`}
         />
 
         <div className="text-center grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mx-auto gap-12 mb-14">
-          <a
-            href="https://nextjs.org/docs"
-            className="hover:text-sky-300 hover:border-sky-300
-            focus:text-sky-300 focus:border-sky-300
-            active:text-sky-300 active:border-sky-300
-             m-4 p-6 text-left text-inherit border border-gray-50 rounded-[10px] transition-colors duration-150"
-          >
-            <h2 className="m-0 mb-4 text-2xl">Documentation &rarr;</h2>
-            <p className="m-0 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+          <Card
+            href="/asdos/daftar-soal"
+            title="Daftar soal"
+            description={`Lihat soal-soal pada praktikum kelas ${user?.kelas}.`}
+          />
         </div>
       </div>
     </PrivateRoute>
