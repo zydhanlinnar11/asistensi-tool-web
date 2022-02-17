f88f5ae5d827c105c3efbcf8022c853f3c3d8da2
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-02-17 23:46:46 +0700

    fix: not redirect to login on asdos pages

--- src/modules/asdos/components/PrivateRoute.tsx
+++ src/modules/asdos/components/PrivateRoute.tsx
@@ -2,13 +2,15 @@ import SpinnerLoading from '@/common/components/elements/SpinnerLoading'
 import { signIn } from '@/modules/auth/auth'
 import { useUser } from '@/modules/auth/providers/UserProvider'
 import Router from 'next/router'
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 
 const PrivateRoute: FC = ({ children }) => {
   const { user, isUserFetched } = useUser()
 
-  if (isUserFetched && !user) signIn()
-  if (isUserFetched && user?.role !== 'asdos') Router.push('/403')
+  useEffect(() => {
+    if (isUserFetched && !user) signIn()
+    else if (isUserFetched && user?.role !== 'asdos') Router.push('/403')
+  }, [isUserFetched, user])
 
   return (
     <div className="my-auto">
