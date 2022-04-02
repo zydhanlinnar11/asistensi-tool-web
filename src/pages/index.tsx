84859ad24b3335d594b5c1748d54e9b4b6dcdb95
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-04-03 02:02:48 +0700

    feat: redirect homepage to scoreboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,1 +1,16 @@
-export { default } from '@/modules/asdos/components/Dashboard'
+import { GetServerSideProps } from 'next'
+
+const HomePage = () => {
+  return <></>
+}
+
+export default HomePage
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  return {
+    redirect: {
+      destination: '/scoreboard/1',
+      permanent: false,
+    },
+  }
+}
